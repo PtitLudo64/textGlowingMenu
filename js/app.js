cff6b1cf7274856a515a2b2439dab305bb12e8c9
PtitLudo64 <ludovic.p.777@gmail.com>
2023-08-15 12:53:48 +0200

    Fine tuning

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@ const modifyMenu = [];
 menuEntries.forEach(entry => {
     const text = entry.innerText.split('');
     const newStr = text.map((letter, idx) => {
+<<<<<<< HEAD
         return `<span style="transition: text-shadow 20ms ${idx*30}ms ">${letter}</span>`;
     })
+=======
+        return `<span style="transition: text-shadow 20ms ${idx*20}ms ">${letter}</span>`;
+    });
+>>>>>>> f2ffca8 (Fine tuning)
     modifyMenu.push(newStr);
 });
 
 menuEntries.forEach( (entry, idx) => {
     entry.innerHTML = modifyMenu[idx].join('');
+<<<<<<< HEAD
 });
+=======
+});
+>>>>>>> f2ffca8 (Fine tuning)
